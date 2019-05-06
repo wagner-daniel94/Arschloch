34b0caab3b21bd861b12b8271afc9dd212b83600
famsad <lorenzschollhorn@gmail.com>
2019-05-06 16:36:09 +0200

    Statistik kann nun aufgerufen werden. Statistic kann nun genutzt werden und erstes Layout für die activity_statistics. Änderung der MainActivity zum aufrufen der Statistiken.

--- app/src/main/java/com/example/arschloch/MainActivity.java
+++ app/src/main/java/com/example/arschloch/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.arschloch;
 
 /*
 Änderung:
-Datum: 17.04.2019
-Autor: Wagner
+Datum: 17.04.2019   / 06.05.2019
+Autor: Wagner       / Schoellhorn
  */
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +19,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button startGameBtn = (Button) findViewById(R.id.startGameBtn);
+        Button openStatsBtn = (Button) findViewById(R.id.openStatsBtn);
 
-        startGameBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,GameActivity.class);
-                startActivity(intent);
-            }
-        });
+        startGameBtn.setOnClickListener(this);
+        openStatsBtn.setOnClickListener(this);
 
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.openStatsBtn:
+                    openActivityStats();
+                    break;
+            case R.id.startGameBtn:
+                    openActivityGame();
+                    break;
+        }
+    }
+
+    public void openActivityStats(){
+        Intent intent = new Intent (this, Statistic.class);
+        startActivity(intent);
+    }
+    public void openActivityGame(){
+        Intent intent = new Intent (this, GameActivity.class);
+        startActivity(intent);
     }
 }
